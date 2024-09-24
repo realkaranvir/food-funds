51eb3377b089c99a385cedbedf4369816d3226e9
realkaranvir <113927390+realkaranvir@users.noreply.github.com>
2024-09-24 11:35:00 -0700

    added express-validator checks for routes

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -23,47 +23,40 @@ const decodeTokenFromRequest = (req) => {
   }
 };
 
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const decodedToken = decodeTokenFromRequest(req);
+
+    const { data, error } = await supabase
+      .from("Items")
+      .select("*")
+      .eq("user_id", decodedToken.user_id);
+
+    if (error) {
+      console.error("Select error: ", error);
+      return res.status(500).json({ message: "Internal server error" });
+    } else {
+      return res.status(200).send(data);
+    }
+  } catch (err) {
+    console.error("Error fetching items: ", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 router.get(
-  "/",
+  "/amount-spent",
   [
     verifyToken,
     check("startDate").isISO8601().withMessage("Invalid startDate format"),
     check("endDate").isISO8601().withMessage("Invalid endDate format"),
   ],
   async (req, res) => {
-    try {
-      const decodedToken = decodeTokenFromRequest(req);
-
-      const { data, error } = await supabase
-        .from("Items")
-        .select("*")
-        .eq("user_id", decodedToken.user_id);
-
-      if (error) {
-        console.error("Select error: ", error);
-        return res.status(500).json({ message: "Internal server error" });
-      } else {
-        return res.status(200).send(data);
-      }
-    } catch (err) {
-      console.error("Error fetching items: ", err);
-      res.status(500).json({ message: "Internal server error" });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  }
-);
 
-router.get(
-  "/amount-spent",
-  [
-    verifyToken,
-    check("name").not().isEmpty().trim().escape(),
-    check("cost").isNumeric().withMessage("Cost must be a number"),
-    check("date_purchased")
-      .isISO8601()
-      .toDate()
-      .withMessage("Invalid date format"),
-  ],
-  async (req, res) => {
     const { startDate, endDate } = req.query;
     try {
       const decodedToken = decodeTokenFromRequest(req);
@@ -97,23 +90,42 @@ router.get(
   }
 );
 
-router.post("/", verifyToken, async (req, res) => {
-  const foodItem = req.body;
-  try {
-    const decodedToken = decodeTokenFromRequest(req);
-    foodItem.user_id = decodedToken.user_id;
-
-    const { error } = await supabase.from("Items").insert(foodItem);
-
-    if (error) {
-      console.error("Error inserting data: ", error);
-      return res.status(500).json({ message: "Internal server error" });
+router.post(
+  "/",
+  [
+    verifyToken,
+    check("name").not().isEmpty().trim().escape(),
+    check("cost").isNumeric().withMessage("Cost must be a number"),
+    check("date_purchased")
+      .isISO8601()
+      .toDate()
+      .withMessage("Invalid date format"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    return res.status(200).json({ message: "Successfully inserted data" });
-  } catch (err) {
-    console.error("Error processing request:", err);
-    return res.status(500).json({ error: "An internal server error occurred" });
+
+    const foodItem = req.body;
+    try {
+      const decodedToken = decodeTokenFromRequest(req);
+      foodItem.user_id = decodedToken.user_id;
+
+      const { error } = await supabase.from("Items").insert(foodItem);
+
+      if (error) {
+        console.error("Error inserting data: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+      return res.status(200).json({ message: "Successfully inserted data" });
+    } catch (err) {
+      console.error("Error processing request:", err);
+      return res
+        .status(500)
+        .json({ error: "An internal server error occurred" });
+    }
   }
-});
+);
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,91 +5,126 @@ const bcrypt = require("bcryptjs");
 const { signToken, verifyToken } = require("../jwt.js");
 const { check, validationResult } = require("express-validator");
 
-router.post("/signup", async (req, res) => {
-  const { username, password, name, email_address } = req.body;
-  try {
-    const hash = await bcrypt.hash(password, 14);
-
-    const { error: insertError } = await supabase.from("Users").insert({
-      username: username,
-      password: hash,
-      name: name,
-      email_address: email_address,
-    });
-
-    if (insertError) {
-      console.error("Error: ", insertError);
-      return res.status(500).json({ message: "Error inserting user" });
+router.post(
+  "/signup",
+  [
+    check("username")
+      .isAlphanumeric()
+      .withMessage("Username must be alphanumeric"),
+    check("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long"),
+    check("email_address").isEmail().withMessage("Invalid email address"),
+    check("name")
+      .not()
+      .isEmpty()
+      .trim()
+      .escape()
+      .withMessage("Name is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const { data: user, selectError } = await supabase
-      .from("Users")
-      .select("*")
-      .eq("username", username)
-      .single();
+    const { username, password, name, email_address } = req.body;
+    try {
+      const hash = await bcrypt.hash(password, 14);
 
-    if (selectError) {
-      console.error("Error: ", selectError);
-      return res.status(500).json({ message: "Error selecting user" });
-    }
+      const { error: insertError } = await supabase.from("Users").insert({
+        username: username,
+        password: hash,
+        name: name,
+        email_address: email_address,
+      });
 
-    const jwtData = {
-      user_id: user.user_id,
-      username: user.username,
-      name: user.name,
-    };
+      if (insertError) {
+        console.error("Error: ", insertError);
+        return res.status(500).json({ message: "Error inserting user" });
+      }
 
-    const token = signToken(jwtData, "30d");
+      const { data: user, selectError } = await supabase
+        .from("Users")
+        .select("*")
+        .eq("username", username)
+        .single();
 
-    return res
-      .status(200)
-      .json({ message: "User created successfully", token: token });
-  } catch (err) {
-    console.error("Error during signup: ", err);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-});
+      if (selectError) {
+        console.error("Error: ", selectError);
+        return res.status(500).json({ message: "Error selecting user" });
+      }
 
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const { data: user, error } = await supabase
-      .from("Users")
-      .select("*")
-      .eq("username", username)
-      .single();
+      const jwtData = {
+        user_id: user.user_id,
+        username: user.username,
+        name: user.name,
+      };
 
-    if (!user) {
+      const token = signToken(jwtData, "30d");
+
       return res
-        .status(401)
-        .json({ message: "Incorrect username or password" });
+        .status(200)
+        .json({ message: "User created successfully", token: token });
+    } catch (err) {
+      console.error("Error during signup: ", err);
+      return res.status(500).json({ message: "Internal server error" });
     }
-
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) {
-      return res
-        .status(401)
-        .json({ message: "Incorrect username or password" });
-    }
-
-    const jwtData = {
-      user_id: user.user_id,
-      username: user.username,
-      name: user.name,
-    };
-    const token = signToken(jwtData, "30d");
-
-    if (error) {
-      console.error("Error: ", error);
-      return res.status(500).json({ message: "Error logging in" });
-    }
-    return res
-      .status(200)
-      .json({ message: "User logged in successfully", token: token });
-  } catch (err) {
-    console.error("Error during login: ", err);
-    return res.status(500).json({ message: "Internal server error" });
   }
-});
+);
+
+router.post(
+  "/login",
+  [
+    check("username").isAlphanumeric().withMessage("Invalid username"),
+    check("password").not().isEmpty().withMessage("Password is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+    try {
+      const { data: user, error } = await supabase
+        .from("Users")
+        .select("*")
+        .eq("username", username)
+        .single();
+
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Incorrect username or password" });
+      }
+
+      const isValid = await bcrypt.compare(password, user.password);
+      if (!isValid) {
+        return res
+          .status(401)
+          .json({ message: "Incorrect username or password" });
+      }
+
+      const jwtData = {
+        user_id: user.user_id,
+        username: user.username,
+        name: user.name,
+      };
+      const token = signToken(jwtData, "30d");
+
+      if (error) {
+        console.error("Error: ", error);
+        return res.status(500).json({ message: "Error logging in" });
+      }
+      return res
+        .status(200)
+        .json({ message: "User logged in successfully", token: token });
+    } catch (err) {
+      console.error("Error during login: ", err);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
 
 module.exports = router;
